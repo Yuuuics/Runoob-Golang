d2dd49ad1e4ffdb7d54b299dc0f60f56a8680820
Yuics <yuics0@icloud.com>
2022-12-30 20:10:15 +0800

    Update '\n'

--- Go 语言运算符/关系运算符.go
+++ Go 语言运算符/关系运算符.go
@@ -13,23 +13,23 @@ func main() {
 	}
 
 	if a < b {
-		fmt.Println("a < b\n")
+		fmt.Println("a < b")
 	} else {
-		fmt.Println("a > b\n")
+		fmt.Println("a > b")
 	}
 
 	if a > b {
-		fmt.Println("a > b\n")
+		fmt.Println("a > b")
 	} else {
-		fmt.Println("a < b\n")
+		fmt.Println("a < b")
 	}
 
 	// Change the value in a and b
 	a = 5
 	b = 20
 	if a <= b {
-		fmt.Println("a <= b\n")
+		fmt.Println("a <= b")
 	} else {
-		fmt.Println("a >= b\n")
+		fmt.Println("a >= b")
 	}
 }
