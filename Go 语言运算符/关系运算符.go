e2691fb73bca7732fa4f478e44472a2cba2fc710
Yuics <yuics0@icloud.com>
2022-12-30 20:06:26 +0800

    关系运算符

---
+++ Go 语言运算符/关系运算符.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 21
+	var b int = 10
+
+	if a == b {
+		fmt.Println("a = b\n")
+	} else {
+		fmt.Println("a != b")
+	}
+
+	if a < b {
+		fmt.Println("a < b\n")
+	} else {
+		fmt.Println("a > b\n")
+	}
+
+	if a > b {
+		fmt.Println("a > b\n")
+	} else {
+		fmt.Println("a < b\n")
+	}
+
+	// Change the value in a and b
+	a = 5
+	b = 20
+	if a <= b {
+		fmt.Println("a <= b\n")
+	} else {
+		fmt.Println("a >= b\n")
+	}
+}
